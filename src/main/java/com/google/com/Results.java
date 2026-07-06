896b8ec9e19e97bd4b17af006b22dc0dcd9fa940
rakeshjavagova <bharathchandragangula99@gmail.com>
2026-07-06 22:36:46 +0530

    removed  comment

--- src/main/java/com/google/com/Results.java
+++ src/main/java/com/google/com/Results.java
@@ -14,9 +14,9 @@ public class Results implements Emcet{
 	
 	
 	public static void main(String[] args) {
-//		System.out.println("ed");
-//		Results s = new Results();
-//		s.getInterMarks(2);
+		System.out.println("ed");
+		Results s = new Results();
+		s.getInterMarks(2);
 		
 		
 	}
